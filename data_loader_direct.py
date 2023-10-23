8f66d4ef744d3bdb2083a074dc761d050108aee2
sergio estevao <smestevao11@gmail.com>
2023-10-23 18:05:09 +0200

    feat: ex4)

--- data_loader_direct.py
+++ data_loader_direct.py
@@ -28,50 +28,68 @@ def read_data_from_file(file_path):
             data.append(entry_data)
         return data
 
-# Load data from the .txt file
-data = read_data_from_file("data/Kindle_Store.txt")
-
-# Establish a connection to the PostgreSQL database using a context manager
-with psycopg2.connect(**db_params) as conn:
+# Function to insert or retrieve a user based on user_id
+def get_or_insert_user(conn, user_id, profile_name):
     with conn.cursor() as cur:
-        for item in data:
-            product_id = item.get("product/productId")
-            title = item.get("product/title")
-            price = item.get("product/price")
+        # Check if the user already exists
+        cur.execute("SELECT user_id FROM user_profile WHERE user_id = %s", (user_id,))
+        existing_user = cur.fetchone()
 
-            if product_id is None:
-                continue
+        if not existing_user:
+            # Insert the user data into the 'user' table
+            cur.execute("INSERT INTO user_profile (user_id, profile_name) VALUES (%s, %s)", (user_id, profile_name))
+        
+        return user_id
 
-            # Check if the product_id already exists
-            cur.execute("SELECT product_id FROM product WHERE product_id = %s", (product_id,))
-            existing_product = cur.fetchone()
+# Function to load data into the database
+def load_data(data):
+    with psycopg2.connect(**db_params) as conn:
+        with conn.cursor() as cur:
+            for item in data:
+                product_id = item.get("product/productId")
+                title = item.get("product/title")
+                price = item.get("product/price")
+                user_id = item.get("review/userId")
+                profile_name = item.get("review/profileName")
 
-            if existing_product is None:
-                # Insert the product data into the 'product' table
+                if product_id is None:
+                    continue
+
+                # Insert or retrieve the user based on user_id
+                user_id = get_or_insert_user(conn, user_id, profile_name)
+
+                # Check if the product_id already exists
+                cur.execute("SELECT product_id FROM product WHERE product_id = %s", (product_id,))
+                existing_product = cur.fetchone()
+
+                if not existing_product:
+                    # Insert the product data into the 'product' table
+                    cur.execute(
+                        "INSERT INTO product (product_id, title, price) VALUES (%s, %s, %s)",
+                        (product_id, title, price)
+                    )
+
+                # Insert review data into the 'review' table
                 cur.execute(
-                    "INSERT INTO product (product_id, title, price) VALUES (%s, %s, %s)",
-                    (product_id, title, price)
+                    """
+                    INSERT INTO review (product_id, user_id, helpfulness, score, review_time, summary, review_text)
+                    VALUES (%s, %s, %s, %s, to_timestamp(%s), %s, %s)
+                    """,
+                    (
+                        product_id,
+                        user_id,
+                        item.get("review/helpfulness"),
+                        item.get("review/score"),
+                        item.get("review/time"),
+                        item.get("review/summary"),
+                        item.get("review/text"),
+                    ),
                 )
 
-            # Insert review data into the 'review' table
-            cur.execute(
-                """
-                INSERT INTO review (product_id, user_id, profile_name, helpfulness, score, review_time, summary, review_text)
-                VALUES (%s, %s, %s, %s, %s, to_timestamp(%s), %s, %s)
-                """,
-                (
-                    product_id,
-                    item.get("review/userId"),
-                    item.get("review/profileName"),
-                    item.get("review/helpfulness"),
-                    item.get("review/score"),
-                    item.get("review/time"),
-                    item.get("review/summary"),
-                    item.get("review/text"),
-                ),
-            )
+        conn.commit()
 
-    # Commit the changes to the database
-    conn.commit()
+if __name__ == "__main__":
+    data = read_data_from_file("data/Kindle_Store.txt")
+    load_data(data)
 
-print("Data loaded successfully.")
+    print("Data loaded successfully.")
--- schema.sql
+++ schema.sql
@@ -4,17 +4,47 @@ CREATE EXTENSION pg_trgm;		-- (similarity , show_Trgm,…, %, <->)
 CREATE TABLE product (
     product_id VARCHAR(255) PRIMARY KEY,
     title TEXT NOT NULL,
-    price MONEY -- You can use an appropriate data type for price
+    price MONEY
+);
+
+CREATE TABLE user_profile (
+    user_id VARCHAR(255) PRIMARY KEY,
+    profile_name TEXT NOT NULL
 );
 
 CREATE TABLE review (
-    review_id SERIAL PRIMARY KEY, -- You can use a serial for a unique identifier
+    review_id SERIAL PRIMARY KEY,
     product_id CHAR(10) REFERENCES product(product_id),
-    user_id VARCHAR(255) NOT NULL,
-    profile_name TEXT,
-    helpfulness VARCHAR(255), -- Assuming it's in the format "X/Y"
-    score DECIMAL(3, 1) NOT NULL, -- Decimal with one decimal place
-    review_time TIMESTAMP NOT NULL, -- Storing as a timestamp
+    user_id VARCHAR(255) REFERENCES user_profile(user_id),
+    helpfulness VARCHAR(255),
+    score DECIMAL(3, 1) NOT NULL,
+    review_time TIMESTAMP NOT NULL,
     summary TEXT,
     review_text TEXT
 );
+
+--INDEXES
+
+-- TRIGRAM
+CREATE INDEX idx_trgm_title ON product USING gin(title gin_trgm_ops);
+
+CREATE INDEX idx_trgm_title_review_text ON user_profile USING gin(profile_name gin_trgm_ops);
+
+CREATE INDEX idx_trgm_summary_review_text ON review USING gin(summary gin_trgm_ops, review_text gin_trgm_ops);
+
+
+
+-- others
+CREATE INDEX idx_review_score ON review (score);
+CREATE INDEX idx_product_price ON product (price);
+CREATE INDEX idx_review_time ON review (review_time);
+
+
+--CONFIGURATIONS
+
+-- Create a new text search configuration named 'se56303'
+CREATE TEXT SEARCH CONFIGURATION se56303 (copy=simple);
+
+-- Assign the 'simple' dictionary to the token types
+ALTER TEXT SEARCH CONFIGURATION se56303
+    ALTER MAPPING FOR asciiword, word, numword, host WITH simple;
